5116784e21acecfda94b85f248bf45056ad690a6
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-12-04 17:40:58 -0300

    Vue 2 to Vue 3

--- Doc/components.js
+++ Doc/components.js
@@ -1,11 +1,13 @@
-Vue.component('todo-item', {
+const appData = {
+    data() {
+        return {
+            todos: [{ id: 0, text: "Sleep" }, { id: 1, text: "Eat" }, { id: 2, text: "Play" }]
+        }
+    }
+}
+const app = Vue.createApp(appData)
+app.component('todo-item', {
     props: ['todo'],
     template: '<li>{{ todo.text }}</li>'
 })
-
-let app = new Vue({
-    el: "#app",
-    data:{
-        todos: [{id: 0, text: "Sleep"}, {id: 1, text: "Eat"}, {id: 2, text: "Play"}]
-    }
-})
+app.mount('#app')
--- Doc/getting-started.js
+++ Doc/getting-started.js
@@ -1,47 +1,61 @@
-let app1 = new Vue({ 
-    el: '#app1',
-    data: {
-        message: 'Hello Word!'
-    }
-});
-
-let app2 = new Vue({
-    el: '#app2',
-    data: {
-        message: "Voiala, the time now is: " + new Date().toLocaleString()
-    }
-})
-
-let app3 = new Vue({
-    el: '#app3',
-    data: {
-        message: "You can see me",
-        seen: true,
-    }
-})
-
-let app4 = new Vue({
-    el: '#app4',
-    data:{
-        todos: [{text: "Become Rich"}, {text: "See Harry Potter"}, {text: "Sleep"}, {text: "Repeat"}]
-    }
-})
-
-let app5 = new Vue({
-    el: '#app5',
-    data: {
-        message: "Click Me !",
-    },
-    methods: {
-        reverse: function(){
-            this.message = this.message.split("").reverse().join("");
+const app1 = {
+    data() {
+        return {
+            message: "Hello Vue 3"
         }
     }
-})
+};
+Vue.createApp(app1).mount('#app1')
 
-let app6 = new Vue({
-    el: '#app6',
-    data: {
-        message: 'Nothing...'
+const app2 = {
+    data() {
+        return {
+            message: "Voiala, the time now is: " + new Date().toLocaleString()
+        }
     }
-})
+}
+Vue.createApp(app2).mount('#app2')
+
+const app3 = {
+    data() {
+        return {
+            message: "You can see me",
+            seen: true
+        }
+    }
+}
+Vue.createApp(app3).mount('#app3')
+
+const app4 = {
+    data() {
+        return {
+            todos: [{ text: "Become Rich" }, { text: "See Harry Potter" }, { text: "Sleep" }, { text: "Repeat" }]
+        }
+    }
+}
+Vue.createApp(app4).mount('#app4');
+
+const app5 = {
+    data() {
+        return {
+            message: "Click Me !",
+        }
+    },
+    methods: {
+
+        reverse() {
+            this.message = this.message.split("").reverse().join("");
+        }
+
+    }
+}
+Vue.createApp(app5).mount('#app5');
+
+const app6 = {
+    data() {
+        return {
+            message: 'Nothing...'
+        }
+    }
+}
+Vue.createApp(app6).mount('#app6');
