9ea38ab6522c503c6c426720b5c5d9caf05efe6d
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-12-05 09:29:25 -0300

    Form Checkbox

--- Doc/form.js
+++ Doc/form.js
@@ -15,3 +15,23 @@ const app2 = Vue.createApp({
     }
 })
 app2.mount('#app2')
+
+const app3 = Vue.createApp({
+    data(){
+        return{
+            checked: false
+        }
+    }
+})
+
+app3.mount('#app3')
+
+const app4 = Vue.createApp({
+    data(){
+        return {
+            names: []
+        }
+    }
+})
+
+app4.mount('#app4')
