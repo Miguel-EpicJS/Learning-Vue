d846d4c508ed6e9a750a813c4a42d6fb6394286e
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-12-05 09:24:48 -0300

    Form Input one line and multiline

---
+++ Doc/form.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            message: 'Edit me'
+        }
+    }
+})
+app.mount('#app')
+
+const app2 = Vue.createApp({
+    data(){
+        return {
+            message: 'Edit me'
+        }
+    }
+})
+app2.mount('#app2')
