5121e552a75f246fcc989596f1f8315bbe004264
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-12-07 10:37:44 -0300

    Create Function
    obs: no back-end

---
+++ Projects/client-crud/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Client CRUD</h1>
+  <create-box ></create-box>
+</template>
+
+<script>
+import createBox from './components/create-box.vue'
+
+export default {
+  components: { createBox },
+  name: 'App',
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Projects/client-crud/src/components/create-box.vue
@@ -0,0 +1,55 @@
+<template>
+  <form @submit.prevent="create">
+    <label for="title-box">Type the name of Client</label>
+    <input v-model="data.nameBox" type="text" id="title-box">
+    <label for="date-box">Select delivery date</label>
+    <input v-model="data.dateBox" type="date">
+    <label for="desc-box">Description for the Client</label>
+    <textarea v-model="data.descBox" id="desc-box" cols="30" rows="10"></textarea>
+    <button type="submit">Create</button>
+  </form>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'create-box',  
+  setup(){
+    const data = reactive({
+      nameBox: '',
+      dateBox: '',
+      descBox: '',
+      clients: [],
+    })
+
+    function create(){
+      data.clients.push({name: data.nameBox, date: data.dateBox, desc: data.descBox})
+      console.log(data.clients)
+    }
+
+    return{
+      data,
+      create,
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+*{
+  font-size: 1.3rem;
+}
+form{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+}
+form label{
+  margin: 20px;
+}
+button{
+  margin-top: 20px;
+}
+</style>
