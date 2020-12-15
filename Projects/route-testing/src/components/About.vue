ee98c190e7822b160e9c0a93c50b3f366db9ba24
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-15 15:50:17 -0300

    Added some text and a name to the About template

--- Projects/route-testing/src/components/About.vue
+++ Projects/route-testing/src/components/About.vue
@@ -1,10 +1,13 @@
 <template>
-  
+  <div>
+      <h1>Hi this is the about page, here we dont have anything for you ;-;</h1>
+      <p>Click here for return to the Home page</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+    name: 'About',
 }
 </script>
 
