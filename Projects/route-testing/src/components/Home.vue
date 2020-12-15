b618ef3a6bd157b54d4a143cf206bfc43af45210
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-15 15:45:42 -0300

    Rename Hello component to the Home component and text change

---
+++ Projects/route-testing/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>Hi everyone, this is the main page.</h1>
+    <p>Click here for go to the about page;</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
