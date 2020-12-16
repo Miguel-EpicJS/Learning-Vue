c1bc4be242d1f5a5321bbed0a26b3c0270440300
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-16 16:48:36 -0300

    now the calculator is working with basic operations

--- Projects/double-calculator/src/App.vue
+++ Projects/double-calculator/src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
     <calc-number @send-value="numbers += $event"></calc-number>
+    <calc-buttons @calc-operation="valid($event)"></calc-buttons>
   </div>
 </template>
 
 <script>
+import CalcButtons from './components/CalcButtons.vue'
 import CalcNumber from './components/CalcNumber.vue'
 
 export default {
   name: 'App',
   data(){
     return{
-      numbers: ''
+      numbers: '',
+      result: ''
+    }
+  },
+  methods:{
+    valid(op){
+      if(op != '='){
+        this.numbers += op
+      }else{
+        this.result = eval(this.numbers)
+      }
     }
   },
   components: {
-    'calc-number': CalcNumber
+    'calc-number': CalcNumber,
+    CalcButtons
   },
   methos:{
     addNumbers(value){
---
+++ Projects/double-calculator/src/components/CalcButtons.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="calc-row">
+      <button @click="submitOperation('/')" class="button-calc">&divide;</button>
+      <button @click="submitOperation('*')" class="button-calc">*</button>
+      <button @click="submitOperation('-')" class="button-calc">-</button>
+      <button @click="submitOperation('+')" class="button-calc">+</button><br>
+      <button @click="submitOperation('=')" class="button-calc">=</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'calc-buttons',
+    emits: ['calc-operation'],
+    methods:{
+        submitOperation(op){
+            this.$emit('calc-operation', op)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.button-calc{
+    width: 100px;
+    height:100px;
+    border-radius: 50%;
+    font-size: 3rem;
+    background: #6000af;
+    color: white;
+    border: 5px solid black;
+    transition: all 500ms;
+    cursor: pointer;
+}
+.button-calc:hover{  
+    background: transparent;
+}
+</style>
