6bdc7adc9657a7518c94e00b01cd1ba391110826
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-19 10:12:03 -0300

    Simple title template

--- Projects/simple-website/src/components/Home.vue
+++ Projects/simple-website/src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <!-- Menu eh -->
+    <!-- Menu here -->
+
+    <div class="container">
+      <div class="title-group">
+        <h1 class="title">Lorem, ipsum dolor.</h1>
+        <p class="title-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Quam maiores dolore excepturi quod molestiae quos?</p>
+      </div>
+    </div>
   </div>
 </template>
 
