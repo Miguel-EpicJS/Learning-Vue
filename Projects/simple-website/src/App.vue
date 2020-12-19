5e8b2eb7eaf077cef7a8b6d067c7783b5fc74ec3
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-19 10:13:05 -0300

    Adjusting layout and colors

--- Projects/simple-website/src/App.vue
+++ Projects/simple-website/src/App.vue
@@ -16,12 +16,16 @@ export default {
 </script>
 
 <style>
+html{
+  background: #11022C;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
+  color: #FFF538;
+  filter: drop-shadow(16px 16px 16px #8C860C);
   margin-top: 60px;
 }
 </style>
