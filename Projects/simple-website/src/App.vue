27b8fd2afdd24b43172166935b012aaf7deb008d
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-19 09:39:34 -0300

    Simple changes,(HelloWorld to Home)

--- Projects/simple-website/src/App.vue
+++ Projects/simple-website/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <home></home>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    home: Home
   }
 }
 </script>
---
+++ Projects/simple-website/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Menu eh -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
