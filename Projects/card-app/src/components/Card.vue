c2fe14a4d288ad0e4f49d92ae9bc173e528b814b
CodigoDeSenior <migueljnm840@gmail.com>
2021-02-04 14:40:54 -0300

    Simples informaçoes para os cards

--- Projects/card-app/src/components/Card.vue
+++ Projects/card-app/src/components/Card.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="card">
-    <h1>This is a card</h1>
+  <div class="cards">
+    <div class="card" v-for="card in cards" :key="card.desc">
+      <!-- <img> here -->
+      <h1> {{card.desc}} </h1>
+      <p> {{card.desc}} </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Card',
+  data(){
+    return {
+      cards: [
+        {
+          title: "Bla",
+          desc: "Blabla",
+          thumb: "url here"
+        }
+      ]
+    }
+  }
 }
 </script>
 
